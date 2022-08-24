f21d6f71bb7de1bc679024c122d3cec24be19dd6
ANGEL <angelxd84130@gmail.com>
2022-08-24 22:35:06 +0800

    Create ElementTree_parse_local.py

---
+++ XML/ElementTree_parse_local.py
@@ -0,0 +1,19 @@
+data1 = '''
+	<users>
+		<user x="2">
+			<id>001</id>
+			<name>Chuck</name>
+		</user>
+		<user x="7">
+			<id>009</id>
+			<name>Brent</name>
+		</user>
+	</users>
+	'''
+stuff = ET.fromstring(data1)
+lst = stuff.findall('users/user')
+print('User count:', len(lst))
+for item in lst:
+	print('Name', item.find('name').text)
+	print('Id', item.find('id).text)
+	print('Attribute', item.get('x'))
